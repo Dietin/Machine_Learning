e7f84fd6c23696e62829e1eed463276006120fbe
Aldi Rhiyadi <115007280+Wazven@users.noreply.github.com>
2023-06-09 07:46:00 +0000

    final API Working

--- main.py
+++ main.py
@@ -26,16 +26,10 @@ def create_db_connection():
 
 # Load model H5
 def load_model():
-    model = tf.keras.models.load_model('something3.h5')
+    model = tf.keras.models.load_model('something3 (1).h5')
     return model
 
 app = FastAPI()
-
-# test=np.array([21,48,155,0,20,1183,1.9],dtype=float) # single row data -> prediksi 1 data orang sesuai dengan atribut yg ada
-# test=np.expand_dims(test,axis=0)
-# new_model = tf.keras.models.load_model('Model/something.h5')
-# print(new_model.predict(test))
-
 
 # Endpoint untuk mendapatkan data pengguna berdasarkan ID pengguna
 @app.get("/predict/{user_id}/{dataUser_id}")
@@ -78,20 +72,23 @@ def predict_calories(user_info: UserInfo, user_id: int = Path(..., description="
 
         # Ambil data pengguna dari database
 
+        print("before")
         query = "SELECT age, weight, height, gender, bmr, activity_level FROM dataUser WHERE user_id = %s AND dataUser_id = %s"
         cursor.execute(query, (user_id, dataUser_id))
         result = cursor.fetchone()
+        
 
         if result is None:
             raise HTTPException(status_code=404, detail="Pengguna dengan ID tersebut tidak ditemukan.")
 
         # Ambil informasi pengguna dari hasil query
-        _, _, _, _, _, _ = result  # Tidak digunakan pada saat prediksi
+        age, weight, height, gender, bmr, activity_level = result  # Tidak digunakan pada saat prediksi
+        print(result)  # Tambahkan baris ini
 
   
         # Gunakan informasi pengguna untuk memprediksi kalori
 
-        input_data = [user_info.age, user_info.weight, user_info.height, user_info.gender, user_info.bmr, user_info.activity_level]
+        input_data = [age, weight, height, gender, bmr, activity_level]
 
         model = load_model()
 
@@ -109,16 +106,27 @@ def predict_calories(user_info: UserInfo, user_id: int = Path(..., description="
             # Tutup koneksi ke database
             cursor.close()
             conn.close()
+            response = {
+                "user_id": user_id,
+                "dataUser_id": dataUser_id,
+                "age": age,
+                "weight": weight,
+                "height": height,
+                "gender": gender,
+                "bmr": bmr,
+                "activity_level": activity_level,
+                "predicted_calories": predicted_calories
+            }
+
+            return response
 
         except Exception as e:
-            raise HTTPException(status_code=500, detail="mysql error")
-
-        return {"predicted_calories": predicted_calories}
+            raise HTTPException(status_code=500, detail="Database Connection Tidak Bekerja")
 
     except Exception as e:
         raise HTTPException(status_code=500, detail="Terjadi kesalahan dalam melakukan prediksi kalori.")
 
-port = 8081
+port = 8000
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=port, timeout_keep_alive=1200)
+    uvicorn.run(app, host="0.0.0.0", port=port)
